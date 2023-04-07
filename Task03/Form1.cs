c25dc28864854ea143b7ced2c43ab9dfeb23a5b8
Akif <Akif@KOMPUTER>
2023-04-07 11:49:32 +0200

    task03

--- Task03/Form1.cs
+++ Task03/Form1.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿//Предположим, что существует прямоугольник, границы
+//которого на 10 пикселей отстоят от границ клиентской
+//области окна. Необходимо при нажатии левой кнопки
+//мыши выводить в заголовок окна сообщение о том, где
+//произошел щелчок мышью: внутри прямоугольника,
+//снаружи или на границе прямоугольника. При нажатии
+//правой кнопки мыши необходимо выводить в заголовок
+//окна информацию о размере клиентской области окна
+//(ширина и высота клиентской области окна).
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +24,37 @@ namespace Task03
         public Form1()
         {
             InitializeComponent();
+            this.Text = "Приложение";
+            this.MouseClick += Form1_MouseClick;
+           
+            
+        }
+
+
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                int x = e.Location.X;
+                int y = e.Location.Y;
+                
+                int height = this.Height;
+                int width = this.Width;
+                if (x == 0 || y == 0 || x == width || y == height)
+                {
+                    this.Text = "Хуй знает что ";
+                }
+                else if (x < (width - 100)  && y < (height - 100))
+                {
+                    this.Text = " внутри прямоугольника ";
+                }
+                else if (x > (width - 100) || y > (height - 100))
+                {
+                    this.Text = " снаружи прямоугольника";
+                }
+            }
+            
         }
     }
 }
